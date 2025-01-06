8a51e17e61d4176c3b92454f3b782c2476ef7675
haerul-s <haerulsaleh.prakom@gmail.com>
2025-01-06 20:39:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import pickle
 import streamlit as st
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 
 model = pickle.load(open('model.pkl', 'rb'))
 
